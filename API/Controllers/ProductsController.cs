fbe6c4197c28df3517c1b2d47ed64f711974dceb
Madan Mohan Reddy Vanga <MVanga@SISFIRST.COM>
2020-05-16 20:12:14 +0530

    API Error handling

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,14 +1,13 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using API.Dtos;
+using API.Errors;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
-using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -44,11 +43,15 @@ namespace API.Controllers
 
     [HttpGet]
     [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
     {
       var spec = new ProductsWithTypesAndBrandsSpecification(id);
 
       var product = await _productRepo.GetEntityWithSpec(spec);
+
+      if (product == null ) return NotFound(new ApiResponse(404));
 
       return _mapper.Map<Product, ProductToReturnDto>(product);
     }
